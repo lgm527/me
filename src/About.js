44d57f3360ab8221103c63af6a0fad509e95ac02
lgm527 <laurellmccaffrey@gmail.com>
2019-10-14 18:13:23 -0400

    updated homepage url

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ function About() {
       <div id='bio'>
         <p>Full stack software developer with a strong enthusiasm for art and connectivity. I discovered web development through working at an auction software company. I bring strong skills in research and writing that help foster team productivity and collaboration. If I’m not in front of a computer or piece of art, you can find me hiking or biking somewhere with more trees than people.</p>
           <h3>Tech Stack</h3>
-          JavaScript, Ruby, Ruby on Rails, React, CSS, HTML5, Git, SQL
+          JavaScript, Ruby, Ruby on Rails, React, CSS3, HTML5, Git, SQL
       </div>
       <img id='me' src={me} alt='me' />
     </div>
