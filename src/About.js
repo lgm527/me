3a6bd715fa779e11cf434f58db0cad7edcc609ae
lgm527 <laurellmccaffrey@gmail.com>
2020-04-28 08:10:43 -0400

    updated bio

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ function About() {
   return (
     <div className="about">
       <div id='bio'>
-        <p>Full stack software engineer with experience in JavaScript/TypeScript, React/Redux and Ruby on Rails. Strong research, communication, and writing skills enable a high level of productivity and collaboration.</p>
+        <p>Full stack software engineer with experience in JavaScript, React, and Ruby on Rails. Strong research, communication, and writing skills enable a high level of productivity and collaboration.</p>
         <h3>Stack</h3>
           JavaScript, TypeScript, React, React Native, Ruby, Ruby on Rails, Python, SQL, PostgreSQL, AWS, Docker, HTML, CSS/SASS, Git, SCRUM/Agile Methodology
       </div>
