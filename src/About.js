e408348f496702437caf89ad64cd8f1082173998
lgm527 <laurellmccaffrey@gmail.com>
2020-04-27 23:07:15 -0400

    photo and hover styles updated

--- src/About.js
+++ src/About.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import me from  './assets/me.jpeg';
+import headshot from  './assets/headshot.jpeg';
 import './style/About.css';
 
 function About() {
   return (
     <div className="about">
       <div id='bio'>
-        <p>Full stack software engineer with experience in JavaScript/TypeScript, React/Redux and Ruby on Rails. Strong research, communication, and writing skills enable a high level of productivity and collaboration. If I’m not in front of a computer or piece of art, you can find me hiking or biking somewhere with more trees than people.</p>
+        <p>Full stack software engineer with experience in JavaScript/TypeScript, React/Redux and Ruby on Rails. Strong research, communication, and writing skills enable a high level of productivity and collaboration.</p>
         <h3>Stack</h3>
           JavaScript, TypeScript, React, React Native, Ruby, Ruby on Rails, Python, SQL, PostgreSQL, AWS, Docker, HTML, CSS/SASS, Git, SCRUM/Agile Methodology
       </div>
-      <img id='me' src={me} alt='me' />
+      <img id='me' src={headshot} alt='me' />
     </div>
   );
 }
