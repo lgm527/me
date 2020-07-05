eaee465fbfcb01ac3a58a7c3871b1698fb16a0dc
lgm527 <laurellmccaffrey@gmail.com>
2020-07-05 13:55:01 -0400

    footer right margin increase

--- src/About.js
+++ src/About.js
@@ -4,11 +4,11 @@ import './style/About.css';
 
 function About() {
   return (
-    <div className="about">
+    <div className='about'>
       <div id='bio'>
-        <p>Full stack software engineer with experience in JavaScript, React, and Ruby on Rails. Strong research, communication, and writing skills enable a high level of productivity and collaboration.</p>
+          <p>Full stack software engineer with experience in JavaScript, React, and Ruby on Rails. Strong research, communication, and writing skills enable a high level of productivity and collaboration.</p>
         <h3>Stack</h3>
-          JavaScript, TypeScript, React, React Native, Ruby, Ruby on Rails, Python, SQL, PostgreSQL, AWS, Docker, HTML, CSS/SASS, Git, SCRUM/Agile Methodology
+          <p>JavaScript, TypeScript, React, React Native, Ruby, Ruby on Rails, Python, SQL, PostgreSQL, AWS, Docker, HTML, CSS/SASS, Git, SCRUM/Agile Methodology</p>
       </div>
       <img id='me' src={headshot} alt='me' />
     </div>
