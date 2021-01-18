0470047f1f722fe34fb53ac39025ee08c23e368c
lgm527 <laurellmccaffrey@gmail.com>
2021-01-17 23:16:28 -0500

    project cards style update

--- src/Work.js
+++ src/Work.js
@@ -11,7 +11,7 @@ class Work extends React.Component {
     name: '',
     description: '',
     demo: '',
-    github: [],
+    github: '',
     img: ''
   }
 
@@ -36,7 +36,7 @@ class Work extends React.Component {
         name: 'TreeTrends2.0',
         description: 'Users can locate trees on the NYC streets near them for stewardship opportunities and email the tree\'s information.',
         demo: 'https://lgm527.github.io/tt2',
-        github: ['https://github.com/lgm527/tt2'],
+        github: 'https://github.com/lgm527/tt2',
         img: tt2
       })
     } else if (project === 'eat') {
@@ -44,7 +44,7 @@ class Work extends React.Component {
         name: 'Eat That Question',
         description: 'A timed trivia game to test your knowledge of Frank Zappa lyrics.',
         demo: 'https://eat-that-question.netlify.app/',
-        github: ['https://github.com/lgm527/eat_that_question'],
+        github: 'https://github.com/lgm527/eat_that_question',
         img: eat
       })
     } else if (project === 'gitstats') {
@@ -52,7 +52,7 @@ class Work extends React.Component {
         name: 'Git Stats',
         description: 'Create a baseball card with GitHub Statistics.',
         demo: 'https://git-stats-and-octocats.netlify.app/',
-        github: ['https://github.com/lgm527/git_stats_2'],
+        github: 'https://github.com/lgm527/git_stats_2',
         img: gitstats
       })
     }
@@ -71,10 +71,6 @@ class Work extends React.Component {
     }
   }
 
-  showGitHubs = () => {
-    return this.state.github.map((link, i) => {return <p key={i}><a href={link} target='_blank' rel='noopener noreferrer' onMouseDown={this.blurMe} onClick={this.blurMe}>Github {i+1}</a></p>})
-  }
-
   blurMe = (event) => {
     event.stopPropagation()
     event.target.blur()
@@ -85,6 +81,7 @@ class Work extends React.Component {
       <div className='container'>
 
       { this.state.show ?
+      
         <div className='show'>
           <h2
           onClick={this.goBack}
@@ -96,47 +93,53 @@ class Work extends React.Component {
           <div id='showText'>
             <h2>{this.state.name}</h2>
             <p key='description'><i>{this.state.description}</i></p>
-            <p key='demo'>
+            <p key='links'>
               <a
                 href={this.state.demo}
                 target='_blank'
                 rel='noopener noreferrer'
                 onMouseDown={this.blurMe}
-                onClick={this.blurMe}
-              >Demo</a>
+                onClick={this.blurMe}>
+              Demo</a>
+              <a 
+                href={this.state.github} 
+                target='_blank' 
+                rel='noopener noreferrer' 
+                onMouseDown={this.blurMe} 
+                onClick={this.blurMe}>
+              Github</a>
             </p>
-            {this.showGitHubs()}
           </div>
         </div>
         :
-            <div className='projects'>
-               <div
-              onClick={() => {this.handleClick('eat')}}
-              onKeyPress={(event) => {this.handleKeyPress(event, 'eat')}}
-              tabIndex={0}
-              className='btn'>
-                <img tabIndex={-1} className='btn_content' id='eat' src={eat} alt='eat that question'/>
-                <h3>Eat That Question</h3>
-              </div>
-
-              <div
-              onClick={() => {this.handleClick('tt2')}}
-              onKeyPress={(event) => {this.handleKeyPress(event, 'tt2')}}
-              tabIndex={0}
-              className='btn'>
-                <img tabIndex={-1} className='btn_content' id='tre' src={tt2} alt='tree2'/>
-                <h3>TreeTrends2.0</h3>
-              </div>  
-
-              <div
-              onClick={() => {this.handleClick('gitstats')}}
-              onKeyPress={(event) => {this.handleKeyPress(event, 'gitstats')}}
-              tabIndex={0}
-              className='btn'>
-                <img tabIndex={-1} className='btn_content' id='gitstats' src={gitstats} alt='gitstats'/>
-                <h3>Git Stats</h3>
-              </div>
+        <div className='projects'>
+            <div
+          onClick={() => {this.handleClick('eat')}}
+          onKeyPress={(event) => {this.handleKeyPress(event, 'eat')}}
+          tabIndex={0}
+          className='btn'>
+            <img tabIndex={-1} className='btn_content' id='eat' src={eat} alt='eat that question'/>
+            <h3>Eat That Question</h3>
           </div>
+
+          <div
+          onClick={() => {this.handleClick('tt2')}}
+          onKeyPress={(event) => {this.handleKeyPress(event, 'tt2')}}
+          tabIndex={0}
+          className='btn'>
+            <img tabIndex={-1} className='btn_content' id='tre' src={tt2} alt='tree2'/>
+            <h3>TreeTrends2.0</h3>
+          </div>  
+
+          <div
+          onClick={() => {this.handleClick('gitstats')}}
+          onKeyPress={(event) => {this.handleKeyPress(event, 'gitstats')}}
+          tabIndex={0}
+          className='btn'>
+            <img tabIndex={-1} className='btn_content' id='gitstats' src={gitstats} alt='gitstats'/>
+            <h3>Git Stats</h3>
+          </div>
+      </div>
       }
 
       </div>
