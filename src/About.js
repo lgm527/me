bfa5b4357f847cd5cc1fd3fa4345a412eb0e0be5
lgm527 <laurellmccaffrey@gmail.com>
2026-04-23 17:46:57 -0400

    add

--- src/About.js
+++ src/About.js
@@ -10,7 +10,7 @@ function About() {
       <h3>Software Engineer</h3>
           <p>If I’m not in front of a computer or piece of art, you can find me hiking, biking, or practicing yoga.</p>
         <h3>Tech Stack</h3>
-          <p>AWS, Azure, GCP, Terraform, Pulumi, CDK, Docker, Python, Go, TypeScript, React, React Native, Ruby/Rails, SQL, PostgreSQL, HTML, CSS/SASS, SCRUM/Agile Methodology</p>
+          <p>AWS, Azure, GCP, Terraform, Docker, Kubernetes, Python, Go, TypeScript, React, React Native, Ruby on Rails, PostgreSQL, SQL, HTML, CSS/SASS, SCRUM/Agile Methodology</p>
       </div>
       <img
       id='me'
